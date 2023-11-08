ff3c65ed679bf56dfb6d6a2eb57eb7aa9f5555f8
huijunnn <huijun.hong@thoughtworks.com>
2023-11-08 17:11:00 +0800

    [huijun]refactor: update Notifier.

--- src/main/java/app/plugin/thoughtworkscodereviewfixtools/intellij/notification/Notifier.java
+++ src/main/java/app/plugin/thoughtworkscodereviewfixtools/intellij/notification/Notifier.java
@@ -7,25 +7,18 @@ import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.Nullable;
 
 public class Notifier {
-    private static final String NOTIFICATION_GROUP_ID = "Custom Notification Group";
-
-    private Notifier() {
-        // 私有构造函数，防止实例化
-    }
+    private Notifier() {}
 
     public static void notifyInfo(@Nullable Project project, String content) {
-        createNotification(content, NotificationType.INFORMATION).notify(project);
+        NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
+                .createNotification(content, NotificationType.INFORMATION)
+                .notify(project);
     }
 
     public static void notifyError(@Nullable Project project, String content) {
-        createNotification(content, NotificationType.ERROR).notify(project);
+        NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
+                .createNotification(content, NotificationType.ERROR)
+                .notify(project);
     }
 
-    private static Notification createNotification(String content, NotificationType type) {
-        return NotificationGroupManager
-                .getInstance().
-                getNotificationGroup(NOTIFICATION_GROUP_ID)
-                .createNotification(content, type);
-    }
 }
-
